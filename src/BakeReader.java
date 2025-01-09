3742d1aa34f0be8e22b52c5721409b26e28a6d5e
Amir <sizewoo@gmail.Com>
2025-01-09 18:22:39 +0100

    fa

--- src/BakeReader.java
+++ src/BakeReader.java
@@ -7,15 +7,23 @@ public class BakeReader {
     private String cheminFichier;
     private HashMap<String, String> variables = new HashMap<>();
     private HashMap<String, String[]> cibles = new HashMap<>();
+    private HashMap<String, String> commandes = new HashMap<>();
 
     public BakeReader(String cheminFichierS) {
         cheminFichier = cheminFichierS;
     }
 
+
+/*-------------------------------------- Lis et met dans des hashmaps les Varibles, cibles et commandes ---------------------------------------------- */
+
+
     public void readInformations() {
         try (BufferedReader lecteur = new BufferedReader(new FileReader(cheminFichier))) {
             String ligne;
+            String cibleAct = null;
             while ((ligne = lecteur.readLine()) != null) {
+                
+
 
                 if (ligne.contains("=")) {
                     String[] parts = ligne.split("=", 2); 
@@ -26,40 +34,64 @@ public class BakeReader {
                     }
                 }
 
-                if (ligne.contains(":") && !ligne.contains("=")) {
+                else if (ligne.contains(":") && !ligne.contains("=")) {
                     String[] parts = ligne.split(":", 2);
                     if (parts.length == 2) {
-                        cibles.put(parts[0].trim(), parts[1].trim().split(" "));
+                        cibleAct = parts[0].trim();
+                        cibles.put(cibleAct, parts[1].trim().split(" "));
+                        
                     }
                 }
+
+                else if (ligne.startsWith("\t")) {
+                    String commande = ligne.trim();
+                    commande = replaceVariables(commande); 
+                    commandes.put(cibleAct, commande);
+                }
+
+
 
             }
         } catch (IOException e) {
             System.err.println("petit soucis fichier bizzare" );
         }
     }
+/*-------------------------------------- remplace les variables par le contenue des variables ---------------------------------------------- */
 
+private String replaceVariables(String commande) {
+    for (String variable : variables.keySet()) {
+        if (commande.contains("$(" + variable + ")")) {
+            commande = commande.replace("$(" + variable + ")", variables.get(variable));
+        }
+    }
+    return commande;
+}
 
+/*-------------------------------------- Afficher pour voir si cca a bien lue le bakefile ---------------------------------------------- */
 
-    public void afficherVariables() {
+    public void afficher() {
         for (String key : variables.keySet()) {
             System.out.println(key + " = " + variables.get(key));
         }
-    }
-
-    public void afficherTargets() {
         System.out.println("Cibles :");
+
         for (String key : cibles.keySet()) {
             System.out.print(key + " : ");
             System.out.println(String.join(", ", cibles.get(key)));
         }
+        System.out.println("Commandes :");
+
+        for (String target : commandes.keySet()) {
+            System.out.println(target + " -> " + commandes.get(target));
+        }
     }
+
+   
 
 /*-----------------------------------------Juste pour test en attendant----------------------------------------- */ 
     public static void main(String[] args) {
         BakeReader bakeReader = new BakeReader(args[0]);
         bakeReader.readInformations();
-        bakeReader.afficherVariables();
-        bakeReader.afficherTargets();
+        bakeReader.afficher();
     }
 }
